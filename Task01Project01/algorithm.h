e8087b049e7d7a0c684879a353342a4223aa6d83
vanikk54 <skugoli@gmail.com>
2025-03-02 17:29:57 +0300

    Update

--- Task01Project01/algorithm.h
+++ Task01Project01/algorithm.h
@@ -1,49 +1,9 @@
-#include "algorithm.h"
 #include <cmath>
 
+int get_max(int* array, int length);
 
+int get_min(int* array, int length);
 
-int get_max(int* array, int length) {
-	int max = array[0];
+double calculate_arithmetical_avg(int* array, int length);
 
-	for (int i = 0; i < length; i++)
-	{
-		if (array[i] > max) {
-			max = array[i];
-		}
-	}
-}
-
-int get_min(int* array, int length) {
-	int min = array[0];
-
-
-	for (int i = 0; i < length; i++)
-	{
-		if (array[i] < min) {
-			min = array[i];
-		}
-	}
-	return 0;
-}
-
-double calculate_arithmetical_avg(int* array, int length) {
-	double s = 0;
-
-	for (int i = 0; i < length; i++)
-	{
-		sum += array[i];
-	}
-
-	return sum / length;
-}
-
-double calculate_geometrical_avg(int* array, int length) {
-	double p = 1;
-
-	for (int i = 0; i < length; i++)
-	{
-		p *= array[i];
-	}
-	return pow(p 1.0, length);
-}
+double calculate_geometrical_avg(int* array, int length);
